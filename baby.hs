1885bc8a6a175dfb31403c5811aaaa2bacc475e6
Christopher David Desjardins <cddesjardins@gmail.com>
2015-09-15 21:19:46 +0000

    haskell ftw

--- baby.hs
+++ baby.hs
@@ -18,5 +18,3 @@ length' xs = sum [1 | _ <- xs]  -- replaces every element of list xs with a 1
 
 -- extract lower case
 extractLowercase st = [c | c <- st, c `elem` ['a'..'z']]
-
-let rightTriangles = [(a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24]
